4e94da0edd299aba9959709ef35fc107fe2344d8
GLUKERZZZ <artem2801200635@gmail.com>
2025-05-18 20:28:20 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -1,27 +1,25 @@
 // Copyright 2022 NNTU-CS
+#pragma once
 #ifndef TRAIN_H
 #define TRAIN_H
 
-#include <vector>
-
 class Train {
 private:
-    struct Car {
-        bool light;
-        Car *next;
-        Car *prev;
-    };
-
-    Car *first;
-    int countOp;
+  struct Car {
+    bool light;
+    Car* next;
+    Car* prev;
+  };
+  int countOp;
+  Car* first;
 
 public:
-    Train();
-    ~Train();
-
-    void addCar(bool light);
-    int getLength(); // реализуем внутри
-    int getOpCount() { return countOp; }
+  Train();
+  ~Train();
+  void addCar(bool light);
+  int getLength();
+  int getOpCount();
 };
 
 #endif // TRAIN_H
+
